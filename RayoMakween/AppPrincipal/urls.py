b7b2723735ae5b1857ee7a23d600d1ba81bddb99
RoberNar <rober@narbona.cl>
2023-05-31 11:01:52 -0400

    Arreglo rutas
    Arreglando rutas(Saltaban de Login a Index)

--- RayoMakween/AppPrincipal/urls.py
+++ RayoMakween/AppPrincipal/urls.py
@@ -1,16 +1,17 @@
 
 from django.urls import path
-from .views import index, login
+from .views import index, login, registro, busqueda
+from .views import crearTrabajo, revisionTrabajo, solicitud, trabajo, cantidadTrabajos, estadoPublicacion
 
 urlpatterns = [
     path('', index,name='index'),
     path('login', login,name='login'),
-    path('registro', index,name='registro'),
-    path('busqueda', login,name='busqueda'),
-    path('crearTrabajo', index,name='crearTrabajo'),
-    path('revisionTrabajo', login,name='revisionTrabajo'),
-    path('solicitud', index,name='solicitud'),
-    path('trabajo', login,name='trabajo'),
-    path('cantidadTrabajos', login,name='cantidadTrabajos'),
-    path('estadoPublicacion', index,name='estadoPublicacion'),   
+    path('registro', registro,name='registro'),
+    path('busqueda', busqueda,name='busqueda'),
+    path('crearTrabajo', crearTrabajo,name='crearTrabajo'),
+    path('revisionTrabajo', revisionTrabajo,name='revisionTrabajo'),
+    path('solicitud', solicitud,name='solicitud'),
+    path('trabajo', trabajo,name='trabajo'),
+    path('cantidadTrabajos', cantidadTrabajos,name='cantidadTrabajos'),
+    path('estadoPublicacion', estadoPublicacion,name='estadoPublicacion'),   
 ]
