7a45fe7b17bb3f6aa11bc5dc00af3e5d0967232e
OctaneMv33 <optimusg0602@gmail.com>
2023-05-31 10:44:28 -0400

    Creado modelo de Estado Publicacion

--- RayoMakween/AppPrincipal/models.py
+++ RayoMakween/AppPrincipal/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class EstadoPublicacion(models.Model):
+    id_estpub = models.IntegerField(db_column = 'id_estpub', primary_key= True)
+    nombre_estpub = models.CharField(db_column = 'nombre_estpub', max_length = 30, blank = False, null = False)
+
+    def _str_(self):
+        return str(self.nombre_estpub)
