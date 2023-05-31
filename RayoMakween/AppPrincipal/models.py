718f8f8056f541f52840a8585147feb800c1ca55
OctaneMv33 <optimusg0602@gmail.com>
2023-05-31 11:00:40 -0400

    Creados Modelos Fuertes
    Creados Modelos: Contacto, Categoria Trabajo y Material

--- RayoMakween/AppPrincipal/models.py
+++ RayoMakween/AppPrincipal/models.py
@@ -7,3 +7,26 @@ class EstadoPublicacion(models.Model):
 
     def _str_(self):
         return str(self.nombre_estpub)
+    
+class CategoriaTrabajo(models.Model):
+    id_categtrabajo = models.IntegerField(db_column = 'id_categtrabajo', primary_key = True)
+    nombre_categtrabajo = models.CharField(db_column = 'nombre_categtrabajo', max_length = 50, blank = False, null = False)
+
+    def _str_(self):
+        return str(self.nombre_categtrabajo)
+
+class Material(models.Model):
+    id_material = models.AutoField(db_column = 'id_material', primary_key = True)
+    nombre_material = models.CharField(db_column = 'nombre_material', max_length = 80, blank = False, null = False)
+
+    def _str_(self):
+        return str(self.nombre_material)
+    
+class Contacto(models.Model):
+    id_contacto = models.AutoField(db_column = 'id_contacto', primary_key = True)
+    correo = models.CharField(db_column = 'correo', max_length = 100, blank = False, null = False)
+    telefono = models.IntegerField(db_column = 'telefono', blank = False, null = False)
+    descripcion = models.CharField(db_column = 'descripcion', max_length = 500, blank = False, null = False)
+
+    def _str_(self):
+        return str(self.correo + self.telefono)
