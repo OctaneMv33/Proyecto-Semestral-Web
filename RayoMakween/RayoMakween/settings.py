db40b9f36daf345bfc0c3b1626853a545017e9c0
RoberNar <rober@narbona.cl>
2023-06-24 17:57:41 -0400

    dsadsa

--- RayoMakween/RayoMakween/settings.py
+++ RayoMakween/RayoMakween/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'RayoMakween.middleware.Redireccion404Middleware',
 ]
 
 ROOT_URLCONF = 'RayoMakween.urls'
